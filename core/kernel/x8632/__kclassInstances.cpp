21c7c6731840ed24a3fe94d4dadcf19d6b184ef2
latentPrion <latentprion@gmail.com>
2010-07-13 22:06:02 -0430

    forgot to #include <in_asm.h> in the .S files since the switchboard files no longer exist.

--- core/kernel/x8632/__kclassInstances.cpp
+++ core/kernel/x8632/__kclassInstances.cpp
@@ -8,9 +8,6 @@
 #include <kernel/common/cpuTrib/cpuTrib.h>
 
 memoryTribC		memoryTrib(
-	static_cast<paddr_t>( CHIPSET_MEMORY___KSPACE_BASE ),
-	static_cast<paddr_t>( CHIPSET_MEMORY___KSPACE_SIZE ),
-	__kspaceReservedMem,
 #ifdef CONFIG_ARCH_x86_32_PAE
 	reinterpret_cast<pagingLevel0S *>( 0xFFFFC000 ),
 #else
