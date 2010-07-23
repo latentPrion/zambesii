50828a7254ae0f425091628e22f9152167062624
latentPrion <latentprion@gmail.com>
2010-07-23 07:29:25 -0400

    Occured to me to add an acknowledgement of both JamesM and Pedigree in the slamCacheC header.

--- core/include/__kclasses/slamCache.h
+++ core/include/__kclasses/slamCache.h
@@ -6,6 +6,25 @@
 	#include <kernel/common/sharedResourceGroup.h>
 	#include <kernel/common/waitLock.h>
 
+/**	NOTES:
+ * This idea, the Slam allocator, is designed by James Molloy for the Pedigree
+ * (http://pedigree-project.org) project. Please make attempts to support
+ * Pedigree.
+ *
+ *	EXPLANATION:
+ * The heap algo works by keeping a free list and a partial list. The partial
+ * list keeps a list of objects which have been free()d. The free list keeps a
+ * list of blocks to be broken down (which have been release by an idle thread)
+ * and can be quickly allocated from.
+ *
+ * One good thing about the Slam algorithm is that its very nature is such that
+ * it caches to the extent that memory is needed. That is, when the heap is
+ * detangled by an idle thread, the derived free blocks are placed on a list
+ * local to that cache.
+ *
+ * The cache then re-uses that block list on the free list indefinitely.
+ **/
+
 class slamCacheC
 :
 public heapCacheC
