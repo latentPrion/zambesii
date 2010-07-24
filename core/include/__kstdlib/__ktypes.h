624b030d589a42f19d0c75af48b124ba0b6c2b06
latentPrion <latentprion@gmail.com>
2010-07-24 18:14:20 -0400

    Separated the kernel error tokens into a separate header file.

--- core/include/__kstdlib/__ktypes.h
+++ core/include/__kstdlib/__ktypes.h
@@ -2,22 +2,9 @@
 	#define __KTYPES_H
 
 	#include <arch/arch.h>
+	#include <__kstdlib/__kerror.h>
 	#include <__kstdlib/compiler/stdint.h>
 	#include <__kstdlib/__kstddef.h>
-
-	#define ERROR_SUCCESS				0
-	#define ERROR_GENERAL				-1
-	#define ERROR_UNKNOWN				-2
-	#define ERROR_CRITICAL				-10
-	#define ERROR_FATAL				-11
-	#define ERROR_INVALID_ARG_VAL			-3
-	#define ERROR_INVALID_ARG			-4
-	#define ERROR_MEMORY_NOMEM			-5
-	#define ERROR_MEMORY___KSPACE_NOMEM		-6
-	#define ERROR_MEMORY_NOMEM_VIRTUAL		-7
-	#define ERROR_MEMORY_NOMEM_PHYSICAL		-8
-	// Hopefully we should never need to use this.
-	#define ERROR_MEMORY_NOMEM_IN_CONSTRUCTOR	-9
 
 #ifndef __ASM__
 
