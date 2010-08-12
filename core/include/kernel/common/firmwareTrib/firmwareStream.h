af181517cc9df203e771a976a7c28d6d4e417cf9
latentPrion <latentprion@gmail.com>
2010-08-12 14:20:45 -0400

    Removed the initializeInterrupts() and *mask*() API from the Firmware Stream.

--- core/include/kernel/common/firmwareTrib/firmwareStream.h
+++ core/include/kernel/common/firmwareTrib/firmwareStream.h
@@ -16,7 +16,7 @@
  *
  * Eventually, the services supported by the kernel through this interface
  * will be:
- *	* Watchdog management.
+ *	* Watchdog management, interrupt controller and vector management.
  *	* Up to four debug devices or services.
  *	* !Memory and !NUMA detection.
  *	* !Power management.
@@ -35,45 +35,6 @@ struct firmwareStreamS
 	error_t (*shutdown)(void);
 	error_t (*suspend)(void);
 	error_t (*awake)(void);
-
-	// Kernel interrupt vector control.
-	/* initializeInterrupts() expects the chipset to place itself in a state
-	 * that will allow the kernel to request masking and unmasking of
-	 * IRQs.
-	 *
-	 * Additionally, the chipset must see to any watchdog timer that it
-	 * has on board. During this call, the chipset is expected to, if it has
-	 * a watchdog, feed it, and register an ISR with the timer tributary to
-	 * enable the kernel to continue to feed it.
-	 *
-	 * This implies that if a chipset has a watchdog, it must initialize a
-	 * continuous timer device during initializeInterrupts(). As a result,
-	 * the chipset is also expected to route that timer IRQ to the
-	 * bootstrap processor, and enable IRQs on the BSP as well.
-	 *
-	 * Note well that Zambezii will *never* statically enable or disable
-	 * IRQs on the BSP until after initializeInterrupts2() has been called.
-	 * The chipset is expected to know WHEN and HOW to
-	 * permanently enable IRQs on the BSP. This can be done at one of two
-	 * times: When initializeInterrupts() is called, the chipset can do
-	 * a local IRQ enable on the BSP, or later on when the kernel calls
-	 * initializeInterrupts2(). If not done up to then, the kernel will just
-	 * enable local IRQs on the BSP and continue.
-	 *
-	 * initializeInterrupts() is called AS SOON AS POSSIBLE after the
-	 * kernel is jumped to to ensure that any watchdog device is fed. At
-	 * that point, there is no access to memory management.
-	 *
-	 * initalizeInterrupts2() is called later on when the kernel is ready to
-	 * set up software ISRs. At this point, the kernel has Memory allocation
-	 * and an operational heap.
-	 **/
-	error_t	(*initializeInterrupts)(void);
-	error_t (*initializeInterrupts2)(void);
-	error_t (*maskSingle)(uarch_t vector);
-	error_t (*maskAll)(void);
-	error_t (*unmaskSingle)(uarch_t vector);
-	error_t (*unmaskAll)(void);
 
 	struct watchdogRivS	*watchdogRiv;
 	struct debugRivS	*debugRiv1;
