da63642a3070b7c3bbe7a2d163613372855c710c
latentPrion <latentprion@gmail.com>
2010-08-18 18:27:19 -0400

    Fixed invlpg inline ASM to use a memory constraint.

--- core/arch/x8632/tlbControl.cpp
+++ core/arch/x8632/tlbControl.cpp
@@ -1,14 +1,29 @@
-#include <arch/x8632/paging.h>
+
 #include <arch/tlbControl.h>
+#include <arch/x8632/paging.h>
+
+void tlbControl::flushSingleEntry(void *vaddr)
+{
+	asm volatile (
+		"invlpg %0 \n\t"
+		:
+		: "m" (vaddr)
+		: "memory"
+	);
+}
 
 void tlbControl::flushEntryRange(void *vaddr, uarch_t nPages)
 {
-	uarch_t		counter, endAddr;
 
-	counter = reinterpret_cast<uarch_t>( vaddr );
-	endAddr = counter + (nPages * PAGING_BASE_SIZE);
-	for (; counter < endAddr; counter += PAGING_BASE_SIZE) {
-		flushSingleEntry(reinterpret_cast<void *>( counter ));
+	for (; nPages > 0;
+		vaddr = reinterpret_cast<void *>(
+			reinterpret_cast<uarch_t>( vaddr ) + PAGING_BASE_SIZE ),
+			nPages--)
+	{
+		asm volatile (
+			"invlpg (%0) \n\t"
+			: "=r" (reinterpret_cast<uarch_t>( vaddr ))
+		);
 	};
 }
 
--- core/include/arch/x8632/tlbControl.h
+++
@@ -1,9 +0,0 @@
-
-inline void tlbControl::flushSingleEntry(void *vaddr)
-{
-	asm volatile (
-		"invlpg (%0) \n\t"
-		: "=r" (vaddr)
-	);
-}
-
