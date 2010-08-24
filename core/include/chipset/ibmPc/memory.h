0fbfcee7c437fb80bb39a3b6ca19bfb13f0d5dcf
latentPrion <latentprion@gmail.com>
2010-08-24 16:33:11 -0400

    Defined a CHIPSET_MEMORY___KBOG_SIZE macro which will determine how the max size per large block allocated by the kernel reservoir's default bog per block allocation.
    Increased ibmPC's CHIPSET_MEMORY___KSPACE_SIZE to 3MB.

--- core/include/chipset/ibmPc/memory.h
+++ core/include/chipset/ibmPc/memory.h
@@ -2,8 +2,8 @@
 	#define _CHIPSET_MEMORY_H
 
 // Absolute load address of the kernel in physical memory.
-#define CHIPSET_MEMORY___KLOAD_PADDR_BASE			(0x100000)
-
+#define CHIPSET_MEMORY___KLOAD_PADDR_BASE		(0x100000)
+#define CHIPSET_MEMORY___KBOG_SIZE			(0x200000)
 
 #endif
 
