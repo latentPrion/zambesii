acc4181cdd4718621d24711e375b4385b4d56ab6
latentPrion <latentprion@gmail.com>
2010-09-19 02:14:33 -0400

    Moved the CPU Trace bitmap for a process from taskS to processS.

--- core/include/kernel/common/process.h
+++ core/include/kernel/common/process.h
@@ -2,6 +2,7 @@
 	#define _PROCESS_H
 
 	#include <__kstdlib/__ktypes.h>
+	#include <__kclasses/bitmap.h>
 	#include <kernel/common/task.h>
 	#include <kernel/common/memoryTrib/memoryStream.h>
 
@@ -12,6 +13,9 @@ struct processS
 	uarch_t		id;
 	taskS		*head;
 	char		*fileName, *filePath, *argString;
+
+	// Tells which CPUs this process has run on.
+	bitmapC		cpuTrace;
 
 	memoryStreamC	*memoryStream;
 };
--- core/include/kernel/common/smpConfig.h
+++ core/include/kernel/common/smpConfig.h
@@ -5,49 +5,21 @@
 	#include <__kclasses/bitmap.h>
 
 /**	EXPLANATION:
- * SMP config and NUMA config work together to inform the scheduler on how to
- * handle pull requests. There is a flag in smpConfigS that indicates whether
- * a logical CPU must check SMP affinity before pulling.
+ * Every thread inherently has SMP config. SMP config is the bitmap of all the
+ * CPUs on which this thread may run. Every time a thread is migrated to a new
+ * CPU, that CPU's bit is set in the process's bitmap of CPUs on which the
+ * process has run. The reason this is important is that it greatly optimizes
+ * TLB shootdown: if we can determine which CPUs a process has run on, we can
+ * avoid unnecessary TLB shootdown on CPUs it hasn't touched yet. The purpose
+ * of the 'last' member is to keep track of the last CPU on which the task was
+ * run: using this, we can avoid having to acquire the lock on the process's
+ * BMP to set the CPU's bit every time a task is scheduled.
  *
- * Every thread has NUMA affinity. The NUMA affinity determines which NUMA CPU
- * banks may pull a task. A bank must pull a task in order for it to be
- * scheduled.
- *
- * In the case of a thread using SMP affinity, after the thread has been pulled
- * by the NUMA bank, its SMP affinity flag is checked, and the kernel sees that
- * the thread has SMP affinity. Therefore only CPUs within the current bank to
- * whom the thread pertains will pull the task.
- *
- * There is no default affinity binding for SMP. If you wish to schedule a task
- * to be run on CPUs X, Y, and Z, yet favour Z, you are out of luck. The chances
- * of a thread being repeatedly pulled by the same bank are only high if that
- * bank is the only bank that it is bound to.
- *
- *	MODEL:
- *	1. Thread with NUMA affinity, and therefore no SMP affinity.
- *		NUMA-bound to: banks 0, 1, 2.
- *			Any CPU within those banks may pull this task.
- *
- *	2. Thread with SMP affinity, and therefore no NUMA affinity.
- *		SMP-bound to: cpus 0, 5, 11, 12, 13 & 18.
- *			In this case, assume that each bank has 4 cpus. Thus
- *			This thread has SMP affinity which spans CPUs in 5
- *			banks.
- *
- *			The kernel will configure this thread to be NUMA-bound
- *			therefore to all banks with whom its SMP config
- *			intersects. This task will be pulled first by any of
- *			those banks, then secondly by one of its preferred CPUs,
- *			which is on the banks that pulled it for that particular
- *			run.
- *
- * That is, for CPU configuration, SMP affinity and NUMA affinity for CPUs are
- * mutually exclusive.
- *
- * FIXME: Will most likely have to change this to be a class rather than a
- * struct later on. Remember the __korientation instance and the fact that
- * constructors are run *after* the kernel orientation process structure is
- * initialized.
+ * A thread which is NUMA bound to a bank for scheduling is implicitly SMP
+ * bound to the CPUs on that bank. When a thread's NUMA CPU config changes,
+ * its SMP config inherently changes as well. That is, the kernel will set/unset
+ * bits in the SMP config for each CPU in the bank, unless the required changes
+ * are much finer grained.
  **/
 
 #define SMPCONFIG_FLAGS_USES_SMP	(1<<2)
