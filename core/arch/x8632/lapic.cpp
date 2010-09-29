805e8dec02830d70be08f771b8a89ba28dcd3baf
latentPrion <latentprion@gmail.com>
2010-09-29 03:07:38 -0400

    I'm working on x86 Local/IO APICs. Currently have code to parse MPFP and MP CFG tables.

---
+++ core/arch/x8632/lapic.cpp
@@ -0,0 +1,45 @@
+
+#include <arch/paddr_t.h>
+#include <arch/x8632/lapic.h>
+#include <arch/x8632/mp.h>
+#include <arch/walkerPageRanger.h>
+#include <__kclasses/debugPipe.h>
+#include <kernel/common/memoryTrib/memoryTrib.h>
+
+
+#define x86_LAPIC_NPAGES		4
+
+static struct x86LapicCacheS	cache;
+
+error_t x86Lapic::mapLapicMem(void)
+{
+	paddr_t		p = x86Mp::getLapicPaddr();
+	void		*v;
+	status_t	nMapped;
+
+	v = (memoryTrib.__kmemoryStream.vaddrSpaceStream
+		.*memoryTrib.__kmemoryStream.vaddrSpaceStream.getPages)(
+		x86_LAPIC_NPAGES);
+
+	if (v == __KNULL)
+	{
+		__kprintf(ERROR x86LAPIC"Failed to get pages to map LAPIC.\n");
+		return ERROR_MEMORY_NOMEM_VIRTUAL;
+	};
+
+	nMapped = walkerPageRanger::mapInc(
+		&memoryTrib.__kmemoryStream.vaddrSpaceStream.vaddrSpace,
+		v, p, x86_LAPIC_NPAGES,
+		PAGEATTRIB_PRESENT | PAGEATTRIB_WRITE | PAGEATTRIB_SUPERVISOR
+		| PAGEATTRIB_CACHE_WRITE_THROUGH);
+
+	if (nMapped < x86_LAPIC_NPAGES)
+	{
+		__kprintf(ERROR x86LAPIC"Failed to map LAPIC at p 0x%X.\n", p);
+		return ERROR_MEMORY_VIRTUAL_PAGEMAP;
+	};
+
+	cache.v = static_cast<ubit32 *>( v );
+	return ERROR_SUCCESS;
+}
+
--- core/arch/x8632/mpCfg.cpp
+++ core/arch/x8632/mpCfg.cpp
@@ -1,9 +1,15 @@
 
 #include <arch/x8632/mp.h>
 #include <arch/walkerPageRanger.h>
+#include <arch/paging.h>
+#include <__kstdlib/__kflagManipulation.h>
+#include <__kstdlib/__kclib/string.h>
 #include <__kstdlib/__kcxxlib/new>
+#include <__kclasses/debugPipe.h>
 #include <kernel/common/memoryTrib/memoryTrib.h>
 
+
+static struct x86_mpCacheS	cache;
 
 /* I want the MP code to produce, in the end, a CPU map and a CPU Config
  * structure.
@@ -23,18 +29,6 @@ x86_mpFpS *x86Mp::findMpFp(void)
 	return chipset_x86FindMpFp();
 }
 
-x86_mpCfgS *x86Mp::getMpCfgTable(x86_mpFpS *mpfp)
-{
-	ubit32		cfgPaddr = mpfp->cfgTablePaddr;
-	x86_mpCfgS	*ret;
-
-	// Map the cfg table into the kernel vaddrspace.
-	ret = new ((memoryTrib.__kmemoryStream.vaddrSpaceStream
-		.*memoryTrib.__kmemoryStream.vaddrSpaceStream.getPages)(1))
-		x86MpCfgS;
-
-	walkerPageRanger::mapInc(
-		ret, cfgPaddr, 
 
 status_t x86Mp::getChipsetDefaultConfig(x86_mpFpS *mpfp)
 {
@@ -50,19 +44,86 @@ status_t x86Mp::getChipsetDefaultConfig(x86_mpFpS *mpfp)
 	return mpfp->features[0];
 }
 
-
-
-ubit32 x86Mp::getLapicPaddr(x86_mpCfgS *cfg)
+x86_mpCfgS *x86Mp::getMpCfgTable(x86_mpFpS *mpfp)
 {
-	// Returns the physical address of the LAPIC per CPU.
-	return cfg->lapicPaddr;
+	ubit32		cfgPaddr = mpfp->cfgTablePaddr;
+	ubit32		cfgNPages;
+	status_t	nMapped;
+	x86_mpCfgS	*ret;
+
+	// Map the cfg table into the kernel vaddrspace.
+	ret = new ((memoryTrib.__kmemoryStream.vaddrSpaceStream
+		.*memoryTrib.__kmemoryStream.vaddrSpaceStream.getPages)(1))
+		x86_mpCfgS;
+
+	nMapped = walkerPageRanger::mapInc(
+		&memoryTrib.__kmemoryStream.vaddrSpaceStream.vaddrSpace,
+		ret, cfgPaddr, 1,
+		// Don't need write perms here.
+		PAGEATTRIB_PRESENT | PAGEATTRIB_SUPERVISOR);
+
+	if (nMapped < 1)
+	{
+		__kprintf(ERROR x86MP"Failed to map MP Config Table.\n");
+		return __KNULL;
+	};
+
+	ret = (x86_mpCfgS *)(
+		(uarch_t)ret + (mpfp->cfgTablePaddr & PAGING_BASE_MASK_LOW));
+
+	cfgNPages = PAGING_BYTES_TO_PAGES(ret->length) + 1;
+
+	// Free cfg table mem, and reallocate enough to hold the whole thing.
+	memoryTrib.__kmemoryStream.vaddrSpaceStream.releasePages(
+		(void *)((uarch_t)ret & PAGING_BASE_MASK_HIGH), 1);
+
+	ret = new ((memoryTrib.__kmemoryStream.vaddrSpaceStream
+		.*memoryTrib.__kmemoryStream.vaddrSpaceStream.getPages)(
+			cfgNPages))
+		x86_mpCfgS;
+
+	nMapped = walkerPageRanger::mapInc(
+		&memoryTrib.__kmemoryStream.vaddrSpaceStream.vaddrSpace,
+		ret, cfgPaddr, cfgNPages,
+		PAGEATTRIB_PRESENT | PAGEATTRIB_SUPERVISOR);
+
+	if (nMapped < static_cast<status_t>( cfgNPages ))
+	{
+		__kprintf(ERROR x86MP"Failed to map MP Config Table.\n");
+		return __KNULL;
+	};
+
+	ret = (x86_mpCfgS *)(
+		(uarch_t)ret + (mpfp->cfgTablePaddr & PAGING_BASE_MASK_LOW));
+
+	// Now we can return the table vaddr.
+	return ret;
 }
 
-ubit16 x86Mp::getNConfigEntries(x86_mpCfgS *cfg)
+void x86Mp::buildCacheData(x86_mpFpS *mpfp, x86_mpCfgS *cfg)
 {
-	return cfg->nEntries;
+	cache.fp = mpfp;
+	cache.cfg = cfg;
+
+	if (cfg != __KNULL)
+	{
+		cache.lapicPaddr = cfg->lapicPaddr;
+		if (__KFLAG_TEST(
+			mpfp->features[0], x86_MPFP_FEAT2_FLAG_PICMODE))
+		{
+			__KFLAG_SET(cache.flags, x86_MPCACHE_FLAGS_WAS_PICMODE);
+		};
+
+		memcpy(cache.oemId, cfg->oemIdString, 8);
+		cache.oemId[8] = '\0';
+		memcpy(cache.oemProductId, cfg->oemProductIdString, 12);
+		cache.oemProductId[12] = '\0';
+	}
+	else
+	{
+		cache.lapicPaddr = 0xFEC00000;
+		cache.oemId[0] = '\0';
+		cache.oemProductId[0] = '\0';
+	};
 }
 
-uarch_t x86Mp::getChipsetNCpus(x86_mpCfgS *cfg)
-{
-	
