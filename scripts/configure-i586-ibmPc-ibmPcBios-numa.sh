10339a5290ff1bfb5afd88c51184af0933faf54c
Latent Prion <LatentPrion@gmail.com>
2025-08-26 21:45:49 -0400

    Scripts: make default config script customizable on cmdline

--- scripts/configure-i586-ibmPc-ibmPcBios-numa.sh
+++ scripts/configure-i586-ibmPc-ibmPcBios-numa.sh
@@ -9,16 +9,38 @@ set -e
 echo "Running autoreconf..."
 autoreconf -i .. ../build/zudi
 
+HOST_TRIPLET="${HOST_TRIPLET:-i586-elf}"
+CPPFLAGS="${CPPFLAGS}"
+CFLAGS="${CFLAGS:--g}"
+CXXFLAGS="${CXXFLAGS:--g}"
+LDFLAGS="${LDFLAGS}"
+if [ -z "${REPLACE_ARGS}" ]
+then REPLACE_ARGS=$(cat <<'EOF'
+    --disable-rt-kernel-irqs
+    --enable-all-debug-opts
+ --disable-kernel-vaddrspace-demand-paging
+ --disable-heap-demand-paging
+ --enable-debug-page-faults
+ --disable-debug-interrupts
+ --enable-debug-locked-interrupt-entry
+ --enable-debug-locks
+    --enable-dtrib-cisternn
+EOF
+)
+fi
+APPEND_ARGS="${APPEND_ARGS:- }"
+
 echo "Running configure with custom options..."
 ../configure \
-    CFLAGS=-g \
-    CXXFLAGS=-g \
-    --host=i586-elf \
+    CPPFLAGS="${CPPFLAGS}" \
+    CFLAGS="${CFLAGS}" \
+    CXXFLAGS="${CXXFLAGS}" \
+    LDFLAGS="${LDFLAGS}" \
+    --host="${HOST_TRIPLET}" \
     ZCHIPSET=pc \
     ZFIRMWARE=bios \
     --enable-scaling=ccnuma \
     --with-max-ncpus=64 \
-    --enable-all-debug-opts \
-    --enable-dtrib-cisternn
+    ${REPLACE_ARGS} ${APPEND_ARGS}
 
 echo "Configuration completed successfully!" 
