f3122788e2a9b167af8e3fefafa79f0268398b81
Latent Prion <latentprion@gmail.com>
2026-03-19 23:06:53 -0400

    LibAcpi: unify table walking functions

--- __kcore/commonlibs/libacpi/madt.cpp
+++ __kcore/commonlibs/libacpi/madt.cpp
@@ -1,137 +1,92 @@
 
 #include <commonlibs/libacpi/madt.h>
 
+namespace
+{
+	struct sMadtEntryHeader
+	{
+		ubit8	type;
+		ubit8	length;
+	};
+
+	static void *getNextEntryOfType(
+		acpiR::sMadt *madt, void **const handle, ubit8 requestedType)
+	{
+		sMadtEntryHeader	*entry;
+
+		if (*handle == NULL) {
+			*handle = ACPI_MADT_GET_FIRST_ENTRY(madt);
+		};
+
+		while (*handle < ACPI_TABLE_GET_ENDADDR(madt))
+		{
+			entry = static_cast<sMadtEntryHeader *>( *handle );
+			if (entry->length == 0) {
+				return NULL;
+			};
+
+			switch (entry->type)
+			{
+			case ACPI_MADT_TYPE_LAPIC:
+			case ACPI_MADT_TYPE_IOAPIC:
+			case ACPI_MADT_TYPE_IRQSOURCE_OVERRIDE:
+			case ACPI_MADT_TYPE_NMI:
+			case ACPI_MADT_TYPE_LAPIC_NMI:
+			case ACPI_MADT_TYPE_LAPICADDR_OVERRIDE:
+			case ACPI_MADT_TYPE_IOSAPIC:
+			case ACPI_MADT_TYPE_LSAPIC:
+			case ACPI_MADT_TYPE_IRQSOURCE:
+				if (entry->type == requestedType)
+				{
+					void *ret = *handle;
+
+					*handle = ACPI_PTR_INC_BY(*handle, entry->length);
+					return ret;
+				};
+
+				break;
+
+			default:
+				break;
+			};
+
+			*handle = ACPI_PTR_INC_BY(*handle, entry->length);
+		};
+
+		return NULL;
+	}
+}
+
 
 acpiR::madt::sCpu *acpiR::madt::getNextCpuEntry(
 	acpiR::sMadt *madt, void **const handle
 	)
 {
-	acpiR::madt::sCpu		*ret=NULL;
-
-	if (*handle == NULL) {
-		*handle = ACPI_MADT_GET_FIRST_ENTRY(madt);
-	};
-
-	while (*handle < ACPI_TABLE_GET_ENDADDR(madt))
-	{
-		switch (ACPI_MADT_GET_TYPE(*handle))
-		{
-		case ACPI_MADT_TYPE_LAPIC:
-			ret = static_cast<acpiR::madt::sCpu *>( *handle );
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			return ret;
-
-
-		default:
-			// Use the 'length' member to skip over all others.
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			break;
-		};
-	};
-
-	return NULL;
+	return static_cast<acpiR::madt::sCpu *>(
+		getNextEntryOfType(madt, handle, ACPI_MADT_TYPE_LAPIC));
 }
 
 acpiR::madt::sIoApic *acpiR::madt::getNextIoApicEntry(
 	acpiR::sMadt *madt, void **const handle
 	)
 {
-	acpiR::madt::sIoApic	*ret=NULL;
-
-	if (*handle == NULL) {
-		*handle = ACPI_MADT_GET_FIRST_ENTRY(madt);
-	};
-
-	while (*handle < ACPI_TABLE_GET_ENDADDR(madt))
-	{
-		switch (ACPI_MADT_GET_TYPE(*handle))
-		{
-		case ACPI_MADT_TYPE_IOAPIC:
-			ret = static_cast<acpiR::madt::sIoApic *>( *handle );
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			return ret;
-
-		default:
-			// Use the 'length' member to skip over all others.
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			break;
-		};
-	};
-
-	return NULL;
+	return static_cast<acpiR::madt::sIoApic *>(
+		getNextEntryOfType(madt, handle, ACPI_MADT_TYPE_IOAPIC));
 }
 
 acpiR::madt::sLapicNmi *acpiR::madt::getNextLapicNmiEntry(
 	acpiR::sMadt *madt, void **const handle
 	)
 {
-	acpiR::madt::sLapicNmi	*ret=NULL;
-
-	if (*handle == NULL) {
-		*handle = ACPI_MADT_GET_FIRST_ENTRY(madt);
-	};
-
-	while (*handle < ACPI_TABLE_GET_ENDADDR(madt))
-	{
-		switch (ACPI_MADT_GET_TYPE(*handle))
-		{
-		case ACPI_MADT_TYPE_LAPIC_NMI:
-			ret = static_cast<acpiR::madt::sLapicNmi *>( *handle );
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			return ret;
-
-		default:
-			// Use the 'length' member to skip over all others.
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			break;
-		};
-	};
-
-	return NULL;
+	return static_cast<acpiR::madt::sLapicNmi *>(
+		getNextEntryOfType(madt, handle, ACPI_MADT_TYPE_LAPIC_NMI));
 }
 
 acpiR::madt::sIrqSourceOver *acpiR::madt::getNextIrqSourceOverrideEntry(
 	acpiR::sMadt *madt, void **const handle
 	)
 {
-	acpiR::madt::sIrqSourceOver	*ret=NULL;
-
-	if (*handle == NULL) {
-		*handle = ACPI_MADT_GET_FIRST_ENTRY(madt);
-	};
-
-	while (*handle < ACPI_TABLE_GET_ENDADDR(madt))
-	{
-		switch (ACPI_MADT_GET_TYPE(*handle))
-		{
-		case ACPI_MADT_TYPE_IRQSOURCE_OVERRIDE:
-			ret = static_cast<acpiR::madt::sIrqSourceOver*>( *handle );
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			return ret;
-
-		default:
-			// Use the 'length' member to skip over all others.
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::madt::sCpu *)*handle)->length);
-
-			break;
-		};
-	};
-
-	return NULL;
+	return static_cast<acpiR::madt::sIrqSourceOver *>(
+		getNextEntryOfType(
+			madt, handle, ACPI_MADT_TYPE_IRQSOURCE_OVERRIDE));
 }
-
--- __kcore/commonlibs/libacpi/srat.cpp
+++ __kcore/commonlibs/libacpi/srat.cpp
@@ -2,70 +2,68 @@
 #include <__kclasses/debugPipe.h>
 #include <commonlibs/libacpi/srat.h>
 
+namespace
+{
+	struct sSratEntryHeader
+	{
+		ubit8	type;
+		ubit8	length;
+	};
+
+	static void *getNextEntryOfType(
+		acpiR::sSrat *srat, void **const handle, ubit8 requestedType)
+	{
+		sSratEntryHeader	*entry;
+
+		if (*handle == NULL) {
+			*handle = ACPI_SRAT_GET_FIRST_ENTRY(srat);
+		};
+
+		while (*handle < ACPI_TABLE_GET_ENDADDR(srat))
+		{
+			entry = static_cast<sSratEntryHeader *>( *handle );
+			if (entry->length == 0) {
+				return NULL;
+			};
+
+			switch (entry->type)
+			{
+			case ACPI_SRAT_TYPE_CPU:
+			case ACPI_SRAT_TYPE_MEM:
+				if (entry->type == requestedType)
+				{
+					void *ret = *handle;
+
+					*handle = ACPI_PTR_INC_BY(*handle, entry->length);
+					return ret;
+				};
+
+				break;
+
+			default:
+				break;
+			};
+
+			*handle = ACPI_PTR_INC_BY(*handle, entry->length);
+		};
+
+		return NULL;
+	}
+}
+
 
 acpiR::srat::sCpu *acpiR::srat::getNextCpuEntry(
 	acpiR::sSrat *srat, void **const handle
 	)
 {
-	acpiR::srat::sCpu		*ret=NULL;
-
-	if (*handle == NULL) {
-		*handle = ACPI_SRAT_GET_FIRST_ENTRY(srat);
-	};
-
-	while (*handle < ACPI_TABLE_GET_ENDADDR(srat))
-	{
-		switch (ACPI_SRAT_GET_TYPE(*handle))
-		{
-		case ACPI_SRAT_TYPE_CPU:
-			ret = static_cast<acpiR::srat::sCpu *>( *handle );
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::srat::sMem *)*handle)->length);
-
-			return ret;
-
-		default:
-			// Use the 'length' member to skip over all others.
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::srat::sMem *)*handle)->length);
-
-			break;
-		};
-	};
-
-	return NULL;
+	return static_cast<acpiR::srat::sCpu *>(
+		getNextEntryOfType(srat, handle, ACPI_SRAT_TYPE_CPU));
 }
 
 acpiR::srat::sMem *acpiR::srat::getNextMemEntry(
 	acpiR::sSrat *srat, void **const handle
 	)
 {
-	acpiR::srat::sMem		*ret=NULL;
-
-	if (*handle == NULL) {
-		*handle = ACPI_SRAT_GET_FIRST_ENTRY(srat);
-	};
-
-	while (*handle < ACPI_TABLE_GET_ENDADDR(srat))
-	{
-		switch (ACPI_SRAT_GET_TYPE(*handle))
-		{
-		case ACPI_SRAT_TYPE_MEM:
-			ret = static_cast<acpiR::srat::sMem *>( *handle );
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::srat::sMem *)*handle)->length);
-
-			return ret;
-
-		default:
-			// Use the 'length' member to skip over all others.
-			*handle = ACPI_PTR_INC_BY(
-				*handle, ((acpiR::srat::sMem *)*handle)->length);
-
-			break;
-		};
-	};
-
-	return NULL;
+	return static_cast<acpiR::srat::sMem *>(
+		getNextEntryOfType(srat, handle, ACPI_SRAT_TYPE_MEM));
 }
-
